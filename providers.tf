5e50f2a3ef414e5c650d8e20ca4736264675ebb6
Lennon <lenodeoliveira@gmail.com>
2024-06-05 20:26:31 -0300

    feat: add bucket s3

--- providers.tf
+++ providers.tf
@@ -4,6 +4,13 @@ terraform {
     aws = ">=3.54.0"
     local = ">=2.1.0"
   }
+
+  backend "s3" {
+    bucket = "my-study-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+  
 }
 
 provider "aws" {
