de38c7a0d3dd184da99c9c6ce8b809d21f0de4ba
Lennon <lenodeoliveira@gmail.com>
2024-06-05 20:16:04 -0300

    feat: modularizing the project

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,14 +1,15 @@
 variable "prefix" {}
 
+variable "vpc_id" {}
+
 variable "cluster_name" {}
 
 variable "retention_days" {}
+
+variable "subnet_ids" { }
 
 variable "desired_size" {}
 
 variable "max_size" {}
 
 variable "min_size" {}
-
-variable "vpc_cidr_block" {}
-
