54aefa389c73c252275b3d40299eaba1f973851f
Lennon <lenodeoliveira@gmail.com>
2024-06-05 19:44:56 -0300

    feat: init project

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+prefix = "lennon"
+cluster_name = "study-trf"
+retention_days = 30
+desired_size = 2
+max_size = 4
+min_size = 2
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {}
+
+variable "cluster_name" {}
+
+variable "retention_days" {}
+
+variable "desired_size" {}
+
+variable "max_size" {}
+
+variable "min_size" {}
